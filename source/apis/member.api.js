b8b5cf59a96dec3358b23b90675284ab1ca2cbc1
ss <asda>
2020-01-03 14:28:54 +0800

    qwe

--- source/apis/member.api.js
+++ source/apis/member.api.js
@@ -233,38 +233,6 @@ export class MemberApi{
         })
     }
 
-    shuxin(json, callback, showLoading = true) {
-
-        if (showLoading)
-            ApiConfig.ShowLoading();
-
-        var header = ApiConfig.GetHeader();
-        console.log(header);
-        console.log(json);
-        wx.request({
-            url: ApiConfig.GetApiUrl() + 'member/shuxin',
-            data: json,
-            method: 'POST',
-            dataType: 'json',
-            header: header,
-            success: function (res) {
-                if (callback != null) {
-                    callback(res.data);
-                }
-            },
-            fail: function (res) {
-                console.log(res);
-                callback(false);
-            },
-            complete: function (res) {
-                console.log(res);
-            
-                if (showLoading)
-                    ApiConfig.CloseLoading();
-            }
-        })
-    }
-
     update(json, callback, showLoading = true) {
 
         if (showLoading)
